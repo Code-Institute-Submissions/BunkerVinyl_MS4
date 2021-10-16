b3a1ba9a2704d1b4a95ea194af60cb429a06d444
PodgeCollins <collinspadraic97@gmail.com>
2021-10-16 11:58:11 +0000

    Set up allauth

--- bunker_vinyl/settings.py
+++ bunker_vinyl/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    # basic users account activites, e.g registration/password resets
+    'allauth.socialaccount',  # logging in with socialmedia accounts
 ]
 
 MIDDLEWARE = [
@@ -59,6 +64,8 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
+                # required by allauth
+                # allows alluth to access http request objects in templates
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -66,6 +73,26 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    # handles superusers logging
+    'django.contrib.auth.backends.ModelBackend',
+
+    # allows users to login using their e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+SITE_ID = 1
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
+ACCOUNT_USERNAME_MIN_LENGTH = 4
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'bunker_vinyl.wsgi.application'
 
