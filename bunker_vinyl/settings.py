035b3029161df10412eeb50cc4c8545cfd0bd498
PodgeCollins <collinspadraic97@gmail.com>
2021-10-16 12:25:22 +0000

    Added home app and templates

--- bunker_vinyl/settings.py
+++ bunker_vinyl/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
     'allauth.account',
     # basic users account activites, e.g registration/password resets
     'allauth.socialaccount',  # logging in with socialmedia accounts
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +61,10 @@ ROOT_URLCONF = 'bunker_vinyl.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
